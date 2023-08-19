9b87e5ff1fac61faea4310f8ab5ffaa55dd0beca
Bayram Enes YILMAZ <63585653+bayramenesyilmaz@users.noreply.github.com>
2023-08-19 23:56:09 +0300

    postman dersi

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const pageRoute = require("./routes/pageRoutes");
+const courseRoute = require("./routes/courseRoutes");
 
 const app = express();
+
+//Connect DB
+mongoose
+  .connect("mongodb://127.0.0.1/smartedu-db")
+  .then(() => console.log("Success connect DB"));
 
 // TEmplate Engine
 app.set("view engine", "ejs");
 
 //Middlewares
-app.use(express.static("public"));
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 //Routes
 app.use("/", pageRoute);
+app.use("/courses", courseRoute);
 
 const port = 3000;
 app.listen(port, () => {

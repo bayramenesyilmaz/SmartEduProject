0b5f6be73bfa2be7f5741c376132a438979f0f42
Bayram Enes YILMAZ <63585653+bayramenesyilmaz@users.noreply.github.com>
2023-08-19 15:43:04 +0300

    route işlemleri

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const pageRoute = require("./routes/pageRoutes");
 
 const app = express();
 
@@ -9,16 +10,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 //Routes
-app.get("/", (req, res) => {
-  res.status(200).render("index", {
-    page_name: "index",
-  });
-});
-app.get("/about", (req, res) => {
-  res.status(200).render("about", {
-    page_name: "about",
-  });
-});
+app.use("/", pageRoute);
 
 const port = 3000;
 app.listen(port, () => {
